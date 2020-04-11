6ab86dc7d20daafe75e1da5341da3e453b02c44a
Nguyen Duc Huy <ndhuy96@gmail.com>
2020-04-11 13:02:03 +0700

    Initial project

---
+++ SoundVisualizer/SoundVisualizer/AppDelegate/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  SoundVisualizer
+//
+//  Created by Nguyen Duc Huy on 4/11/20.
+//  Copyright © 2020 sun. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ SoundVisualizer/SoundVisualizer/Scenes/Main/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  SoundVisualizer
+//
+//  Created by Nguyen Duc Huy on 4/11/20.
+//  Copyright © 2020 sun. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
